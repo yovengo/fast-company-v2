264054b189ce4b8a6c2c0703a7adda148d3ba7a5
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-17 13:11:07 +0300

    display & redirect hotfix

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import UserProvider from "../hooks/useUsers";
+import { useAuth } from "../hooks/useAuth";
+
 const Users = () => {
     const params = useParams();
     const { userId, edit } = params;
+    const { currentUser } = useAuth();
     return (
         <>
             <UserProvider>
                 {userId ? (
-                    edit ? (
-                        <EditUserPage />
+                    edit ? (userId === currentUser._id
+                        ? <EditUserPage /> : <Redirect to={`/users/${currentUser._id}/edit`}/>
                     ) : (
                         <UserPage userId={userId} />
                     )
