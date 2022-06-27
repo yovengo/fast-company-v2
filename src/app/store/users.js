f32efef87a2437cf9015e34c8905412371ab062f
Daniil Ognevoy <roxxor503@gmail.com>
2022-06-27 21:15:09 +0300

    added signUp & createUser methods

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -1,12 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
 import UserService from "../services/user.service";
+import authService from "../services/auth.service";
+import localStorageService from "../services/localStorage.service";
+import getRandomInt from "../utils/getRandomInt";
 
 const usersSlice = createSlice({
     name: "users",
     initialState: {
         entities: null,
         isLoading: true,
-        error: null
+        error: null,
+        auth: null,
+        isLoggedIn: false
     },
     reducers: {
         usersRequested: (state) => {
@@ -19,27 +24,78 @@ const usersSlice = createSlice({
         usersRequestFailed: (state, action) => {
             state.error = action.payload;
             state.isLoading = false;
+        },
+        authRequestSuccess: (state, action) => {
+            state.auth = { ...action.payload, isLoggedIn: true };
+        },
+        authRequestFailed: (state, action) => {
+            state.error = action.payload;
+        },
+        userCreated: (state, action) => {
+            if (!Array.isArray(state.entities)) {
+                state.entities = [];
+            }
+            state.entities.push(action.payload);
         }
     }
 });
 
 const { reducer: usersReducer, actions } = usersSlice;
-const { usersRequested, usersReceived, usersRequestFailed } = actions;
+const { usersRequested, usersReceived, usersRequestFailed, authRequestSuccess, authRequestFailed, userCreated } = actions;
+
+const authRequested = createAction("users/authRequested");
+const userCreateRequested = createAction("users/userCreateRequested");
+const createUserFailed = createAction("users/createUserFailed");
+
+export const signUp = ({ email, password, ...rest }) => async (dispatch) => {
+    dispatch(authRequested);
+    try {
+        const data = await authService.register({ email, password });
+        localStorageService.setTokens(data);
+        dispatch(authRequestSuccess({ userId: data.localId }));
+        dispatch(createUser({
+            _id: data.localId,
+            email,
+            rate: getRandomInt(1, 5),
+            completedMeetings: getRandomInt(0, 200),
+            image: `https://avatars.dicebear.com/api/avataaars/${(
+                Math.random() + 1
+            )
+                .toString(36)
+                .substring(7)}.svg`,
+            ...rest
+        }));
+    } catch (error) {
+        dispatch(authRequestFailed(error.message));
+    }
+};
+
+function createUser(payload) {
+    return async function (dispatch) {
+        dispatch(userCreateRequested());
+        try {
+            const { content } = await UserService.create(payload);
+            dispatch(userCreated(content));
+        } catch (error) {
+            dispatch(createUserFailed(error.message));
+        }
+    };
+}
 
 export const loadUsersList = () => async (dispatch) => {
-        dispatch(usersRequested());
-        try {
-            const { content } = await UserService.get();
-            dispatch(usersReceived(content));
-        } catch (error) {
-            dispatch(usersRequestFailed(error.message));
-        }
+    dispatch(usersRequested());
+    try {
+        const { content } = await UserService.get();
+        dispatch(usersReceived(content));
+    } catch (error) {
+        dispatch(usersRequestFailed(error.message));
+    }
 };
 
 export const getUserById = (userId) => (state) => {
-  if (state.users.entities) {
-    return state.users.entities.find((u) => u._id === userId);
-  }
+    if (state.users.entities) {
+        return state.users.entities.find((u) => u._id === userId);
+    }
 };
 
 export const getUsersList = () => (state) => state.users.entities;
